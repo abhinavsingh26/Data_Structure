8bbf4294c933011b1efeb97f78bf3783ffbd4385
Abhinav Singh <abhinavsingh26@gmail.com>
2021-02-09 09:29:48 +0530

    Add files via upload

---
+++ linked_list.py
@@ -0,0 +1,94 @@
+class Node():
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+
+class Linked_list():
+    def __init__(self):
+        self.head = None
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data) + '-->'
+            itr = itr.next
+
+        print(llstr)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+
+    def insert_at(self, index, data):
+        if index < 0 or index > self.get_length():
+            raise Exception("Index is not valid")
+
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index- 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
+
+    def remove_at(self, index):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index -1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+
+if __name__ == '__main__':
+    ll = Linked_list()
+    ll.insert_values(["India", "Australia", "Brazil", "UK", "USA"])
+    ll. insert_at(0, "Bhutan")
+    ll.remove_at(3)
+    ll.print()
+    ll.insert_values([23,54,65,34,64,76,87,34,65])
+    ll.insert_at_end(789)
+    ll.print()
